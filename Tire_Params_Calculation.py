015042e5a456bb8c2f160202494339b27757452c
mmar <manuel.mar@utec.edu.pe>
2024-03-07 15:58:17 -0500

    tir files

---
+++ Tire_Params_Calculation.py
@@ -0,0 +1,17 @@
+
+#Longitudinal Force 
+
+b0  =    # Shape Factor
+b1  =    # Load influence on longitudinal fricttion coefficient
+b2  =    # Longitudinal friction coefficient 
+b3  =    # Curvature factor of stiffness/load
+b4  =    # Change of stiffness with slip	
+b5  =    # Change of progressivity of stiffness/load	
+b6  =    # Curvature change with load^2
+b7  =    # Curvature change with load	
+b8  =    # Curvature Factor
+b9  =    # Load Influence on Horizontal Shift 
+b10 =    # Horizontal shift	
+b11 =    # Vertical shift	
+b12 =    # Vertical shift at load = 0
+b13 =    # Curvature shift	
